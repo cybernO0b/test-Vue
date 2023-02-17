8bb72488a5724b6b8a9688effcee3cfec869099c
cybernO0b <dm.lashkevich@gmail.com>
2023-02-17 07:47:44 +0500

    test-crm-vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <component :is="layout">
+    <router-view />
+  </component>
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script> 
+import EmptyLayout from '@/layouts/EmptyLayout.vue'
+import MainLayout from '@/layouts/MainLayout.vue'
+export default {
+  computed: {
+    layout() {
+      console.log(this.$route.meta)
+      return (this.$route.meta.layout || 'empty') + '-layout'
     }
+  },
+  components: {
+    EmptyLayout, MainLayout
   }
 }
+</script>
+
+<style lang="scss">
+@import '~materialize-css/dist/css/materialize.min.css';
 </style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="app-main-layout">
+        <nav class="navbar orange lighten-1">
+
+        </nav>
+        <ul class="sidenav app-sidenav open">
+
+        </ul>
+
+        <main class="app-content">
+            <div class="app-page">
+                <router-view />
+            </div>
+        </main>
+
+<div class="fixed-action-btn">
+    <a class="btn-floating btn-large blue" href="#">
+        <i class="large material-icons">add</i>
+    </a>
+</div>
+    </div>
+</template>
