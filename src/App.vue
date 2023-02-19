b13abb726126463444084ea63de881435f538133
cybernO0b <dm.lashkevich@gmail.com>
2023-02-20 04:34:10 +0500

    add more page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
     <component :is="layout">
-    <router-view />
-  </component>
+      <router-view/>
+    </component>
   </div>
 </template>
 
-<script> 
-import EmptyLayout from '@/layouts/EmptyLayout.vue'
-import MainLayout from '@/layouts/MainLayout.vue'
+<script>
+import EmptyLayout from '@/layouts/EmptyLayout'
+import MainLayout from '@/layouts/MainLayout'
 export default {
   computed: {
     layout() {
-      console.log(this.$route.meta)
       return (this.$route.meta.layout || 'empty') + '-layout'
     }
   },
@@ -22,6 +21,8 @@ export default {
 }
 </script>
 
+
 <style lang="scss">
 @import '~materialize-css/dist/css/materialize.min.css';
+@import 'assets/index.css';
 </style>
